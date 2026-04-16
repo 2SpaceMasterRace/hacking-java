9c471ddbd6d05c4b2d278e9fd7dce7caa60bb531
2SpaceMasterRace <alphanovachrono@gmail.com>
2026-04-16 19:12:39 -0400

    Finished challenges in First Steps Chapter

---
+++ Modern Java/lib/first_steps.java
@@ -0,0 +1,29 @@
+void challengeOne(){
+    IO.println("Hari Varsha  V");
+    IO.println("Hari Varsha V");
+}
+
+void challengeTwo() {
+    IO.println("A");
+    //IOprintln("B");
+    IO.println("C");//
+    IO.println("D");
+    /*
+    IO.println("E");
+    IO.println("F");*/
+    IO.println("G");
+}
+
+void challengeThree() {
+    IO.println("Apple");
+    IO.println("Banana");
+    IO.println("Clementine");
+    IO.println("Durian");
+}
+
+
+void main(){
+    challengeOne();
+    challengeTwo();
+    challengeThree();
+}
